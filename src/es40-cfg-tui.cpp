3a792b0d44b3c5561831dd436d8ba73d153fdfda
Martin Vorländer <martin.vorlaender@t-online.de>
2026-08-17 14:08:25 +0200

    Changed header comment and license.

--- src/es40-cfg-tui.cpp
+++ src/es40-cfg-tui.cpp
@@ -1,34 +1,40 @@
 /* ES40 emulator.
- * Copyright (C) 2007-2008 by the ES40 Emulator Project
+ * Copyright (C) 2026 by the ES40 Emulator Project
+ * Copyright (C) 2025 by Kisara Development LLC.
+ * All rights reserved.
  *
- * WWW    : http://es40.org
- * E-mail : camiel@es40.org
+ * WWW    : https://github.com/ES40-Emu/es40
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * SPDX-License-Identifier: BSD-1-Clause
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
- * Although this is not required, the author would appreciate being notified of,
- * and receiving any modifications you may make to the source code that might serve
- * the general public.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 /**
  * \file
  * Configuration file editor, TUI variant.
+ * This still is work in progress!
  *
  * $Id$
  *
+ * X-0.2        Martin Vorländer                                17-AUG-2028
+ *      Changed header comment and license.
  * X-0.1        Martin Vorländer                                01-JUL-2026
  *      File created.
  **/
